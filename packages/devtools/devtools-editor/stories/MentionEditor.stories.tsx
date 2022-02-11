8d9541bb811298530c15fdb3c43ccaeadcc2a8a4
Zarco Nontol <zarconontol@gmail.com>
2022-02-11 12:47:27 -0300

    add mention editor by remirror

--- packages/devtools/devtools-editor/stories/MentionEditor.stories.tsx
+++ packages/devtools/devtools-editor/stories/MentionEditor.stories.tsx
@@ -2,18 +2,49 @@
 // Copyright 2022 DXOS.org
 //
 
-import React  from 'react';
+import React from "react";
 
-import { MentionEditor } from '../src';
+import { MentionEditor } from "../src";
 
 export default {
-  title: 'devtools/MentionEditor'
+  title: "devtools/MentionEditor",
 };
 
-export const Primary  = () => {
+const initialCommandsList = [
+  {
+    label: "select",
+    id: "select",
+  },
+  {
+    label: "children",
+    id: "children",
+  },
+  {
+    label: "links",
+    id: "links",
+  },
+  {
+    label: "parent",
+    id: "parent",
+  },
+  {
+    label: "source",
+    id: "source",
+  },
+  {
+    label: "target",
+    id: "target",
+  },
+  {
+    label: "filter",
+    id: "filter",
+  },
+];
+
+export const Primary = () => {
   return (
     <div>
-      <MentionEditor />
+      <MentionEditor placeholder="Start quering!" commands={initialCommandsList} />
     </div>
   );
 };
