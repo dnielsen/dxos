1ff89dfca8dfde5299a84d6e682c523cc172a2ff
Zarco Nontol <zarconontol@gmail.com>
2022-02-10 21:18:01 -0300

    add remirror

---
+++ packages/devtools/devtools-editor/stories/MentionEditor.stories.tsx
@@ -0,0 +1,19 @@
+//
+// Copyright 2022 DXOS.org
+//
+
+import React  from 'react';
+
+import { MentionEditor } from '../src';
+
+export default {
+  title: 'devtools/MentionEditor'
+};
+
+export const Primary  = () => {
+  return (
+    <div>
+      <MentionEditor />
+    </div>
+  );
+};
