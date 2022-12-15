8eb10567b8d6eef5f69f458a4a2fa71e63ef30f9
Josiah Witt <josiah@witt.life>
2022-12-15 14:30:37 -0500

    refactor(react-client): cleanup all hooks

--- packages/sdk/react-client/src/client/useStatus.ts
+++ packages/sdk/react-client/src/client/useStatus.ts
@@ -2,7 +2,8 @@
 // Copyright 2022 DXOS.org
 //
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import useInterval from 'use-interval';
 
 import { asyncTimeout } from '@dxos/async';
 import { log } from '@dxos/log';
@@ -12,21 +13,17 @@ import { useClient } from './ClientContext';
 export const useStatus = (polling = 1_000) => {
   const [status, setStatus] = useState<boolean>(true);
   const client = useClient();
-  useEffect(() => {
-    // TODO(burdon): Generally check mounted before update.
-    const i = setInterval(async () => {
-      try {
-        // TODO(burdon): Logging doesn't show up for this class.
-        await asyncTimeout(client.getStatus(), 500);
-        setStatus(true);
-      } catch (err) {
-        log.error('heartbeat stalled');
-        setStatus(false);
-      }
-    }, polling);
 
-    return () => clearInterval(i);
-  }, []);
+  useInterval(async () => {
+    try {
+      // TODO(burdon): Logging doesn't show up for this class.
+      await asyncTimeout(client.getStatus(), 500);
+      setStatus(true);
+    } catch (err) {
+      log.error('heartbeat stalled');
+      setStatus(false);
+    }
+  }, polling);
 
   return status;
 };
